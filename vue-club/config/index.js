83a11a8dbb6e1cc37d2e7801dbb45962193ad364
Seaming <347552878@qq.com>
2023-06-27 19:34:22 +0800

    不重要的修改

--- vue-club/config/index.js
+++ vue-club/config/index.js
@@ -1,42 +1,42 @@
-"use strict";
+'use strict'
 // Template version: 1.3.1
 // see http://vuejs-templates.github.io/webpack for documentation.
 
-const path = require("path");
+const path = require('path')
 
 module.exports = {
   dev: {
     // Paths
-    assetsSubDirectory: "static",
-    assetsPublicPath: "/",
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
     proxyTable: {
-      "/api": {
-        target: "http://192.168.43.162:8081",
+      '/api': {
+        target: 'http://192.168.43.162:8081',
         // target: 'http://localhost:8010',
         changeOrigin: true,
         pathRewrite: {
-          "^/api": "/club/api"
+          '^/api': '/club/api'
         }
       },
-      "/admin": {
-        target: "http://192.168.43.162:8082",
+      '/admin': {
+        target: 'http://192.168.43.162:8082',
         changeOrigin: true,
         pathRewrite: {
-          "^/admin": "/club/admin"
+          '^/admin': '/club/admin'
         }
       },
-      "/fileServer": {
-        target: "http://192.168.43.162:8013",
+      '/fileServer': {
+        target: 'http://192.168.43.162:8013',
         // target: 'http://localhost:8010',
         changeOrigin: true,
         pathRewrite: {
-          "^/fileServer": "/"
+          '^/fileServer': '/'
         }
       }
     },
 
     // Various Dev Server settings
-    host: "localhost", // can be overwritten by process.env.HOST
+    host: 'localhost', // can be overwritten by process.env.HOST
     port: 80, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
@@ -56,7 +56,7 @@ module.exports = {
      */
 
     // https://webpack.js.org/configuration/devtool/#development
-    devtool: "cheap-module-eval-source-map",
+    devtool: 'cheap-module-eval-source-map',
 
     // If you have problems debugging vue-files in devtools,
     // set this to false - it *may* help
@@ -68,19 +68,19 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, "../dist/index.html"),
+    index: path.resolve(__dirname, '../dist/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, "../dist"),
-    assetsSubDirectory: "static",
-    assetsPublicPath: "./",
+    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsSubDirectory: 'static',
+    assetsPublicPath: './',
     proxyTable: {
-      "/api": {
-        target: "http://localhost:8081",
+      '/api': {
+        target: 'http://localhost:8081',
         // target: 'http://localhost:8010',
         changeOrigin: true,
         pathRewrite: {
-          "^/api": "/club/api"
+          '^/api': '/club/api'
         }
       }
       // '/api': 'http://localhost:8010/club/api/'
@@ -91,14 +91,14 @@ module.exports = {
 
     productionSourceMap: false,
     // https://webpack.js.org/configuration/devtool/#production
-    devtool: "#source-map",
+    devtool: '#source-map',
 
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
     // Before setting to `true`, make sure to:
     // npm install --save-dev compression-webpack-plugin
     productionGzip: true,
-    productionGzipExtensions: ["js", "css"],
+    productionGzipExtensions: ['js', 'css'],
 
     // Run the build command with an extra argument to
     // View the bundle analyzer report after build finishes:
@@ -106,4 +106,4 @@ module.exports = {
     // Set to `true` or `false` to always turn it on or off
     bundleAnalyzerReport: process.env.npm_config_report
   }
-};
+}
