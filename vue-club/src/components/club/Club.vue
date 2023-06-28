598ef204efa6aea1a76a379fed48aed42fd6e910
zhaodaodao <87349308+zhaodaodao@users.noreply.github.com>
2023-06-28 17:13:59 +0800

    btn_new

--- vue-club/src/components/club/Club.vue
+++ vue-club/src/components/club/Club.vue
@@ -1,13 +1,20 @@
 <template>
-  <div class="" v-if="club != null">
-    <div class="background">
+  <div class="background" v-if="club != null">
+    <img class="gif1" :src="require('../../../res/2.gif')" />
+    <div class="">
       <div class="transbox">
-        <h1>{{ club.name }}</h1>
+        <h1 class="clubName">{{ club.name }}</h1>
         <h2>
           社团简介:
           <div v-html="club.introduce" class="content" />
-          <img v-for="url in imageUrls" :src="url.src" :key="url.src" />
+          <img
+            class="img"
+            v-for="url in imageUrls"
+            :src="url.src"
+            :key="url.src"
+          />
         </h2>
+        <button class="apply">申请加入</button>
       </div>
     </div>
   </div>
@@ -97,5 +104,69 @@ a.cc:visited {
 }
 a.cc:hover {
   color: #ffcc00;
+}
+/* .background {
+  background-image: url('../../../res/1.png'), url('../../../res/2.png'),
+    url('../../../res/3.png'), url('../../../res/4.png'),
+    url('../../../res/5.png');
+
+  background-repeat: no-repeat;
+
+  background-size: cover, 234px 203px, 244px 300px, 310px 210px, 250px 150px;
+
+  background-position: center, left top, right bottom, left bottom, 600px 100px;
+
+  width: 100vw;
+  height: 100vh;
+} */
+
+.img {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 126.361px;
+  flex-shrink: 0;
+}
+
+.gif1 {
+  position: fixed; /* 使用 fixed 定位，使元素始终悬浮在视口的特定位置 */
+  top: 10px; /* 距离顶部的距离，根据需要进行调整 */
+  right: 10px; /* 距离右侧的距离，根据需要进行调整 */
+  z-index: 1000; /* 设置较高的 z-index 值以确保元素在其他元素之上 */
+  width: 150px;
+  height: 150px;
+}
+
+.clubName {
+  color: var(--colors-white, #000000);
+  text-shadow: 0px 4px 4px 0px #e51515bb;
+  font-size: 45px;
+  font-family: ZCOOL KuaiLe;
+  line-height: 70px;
+  letter-spacing: -1px;
+  transform: rotate(-8.672deg);
+}
+
+.content {
+  color: #000;
+  font-size: 18px;
+  font-family: LingWai SC;
+  font-weight: 500;
+  line-height: 130%;
+}
+
+.apply {
+  width: 269px;
+  height: 68px;
+  flex-shrink: 0;
+  border-radius: 24px;
+  background: #a5a6f6;
+  box-shadow: -1px -9px 20px 0px rgba(0, 0, 0, 0.23) inset,
+    1px 9px 24px 0px rgba(125, 125, 237, 0.6) inset, 0px 2px 11px 0px #9394db;
+  color: #fff;
+  font-size: 24px;
+  font-family: Roboto;
+  font-weight: 600;
 }
 </style>
