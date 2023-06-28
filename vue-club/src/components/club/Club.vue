31f4b5f5a9941bafb316c43e1d60de39e58d0fd8
Seaming <347552878@qq.com>
2023-06-28 15:12:56 +0800

    修改社团显示的问题,待添加样式

--- vue-club/src/components/club/Club.vue
+++ vue-club/src/components/club/Club.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="" v-if="club != null">
-    <span class="title">{{ club.name }}</span>
-    <div v-html="club.introduce" class="content"></div>
+    <div class="background">
+      <div class="transbox">
+        <h1>{{ club.name }}</h1>
+        <h2>
+          社团简介:
+          <div v-html="club.introduce" class="content" />
+          <img v-for="url in imageUrls" :src="url.src" :key="url.src" />
+        </h2>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,7 +18,8 @@ const OK = 200
 export default {
   data() {
     return {
-      club: {}
+      club: {},
+      imageList: [] // 社团图片列表
     }
   },
   methods: {
@@ -19,7 +28,16 @@ export default {
       this.$axios.get('/api/clubs/' + num).then(res => {
         if (res.data.code === OK) {
           this.club = res.data.data
-          console.log(this.club)
+        } else {
+          this.$layer.alert(res.data.data)
+        }
+      })
+
+      this.$axios.get(`/other/files/club/${num}`).then(res => {
+        const { code, data } = res.data
+
+        if (code === OK) {
+          this.imageList = data
         } else {
           this.$layer.alert(res.data.data)
         }
@@ -29,6 +47,15 @@ export default {
   created() {
     this.get()
     this.$store.dispatch('screen/setToFull')
+  },
+  computed: {
+    imageUrls() {
+      return this.imageList.map(item => {
+        return {
+          src: `/fileServer/fileServer/${item.filePath}`
+        }
+      })
+    }
   },
   watch: {
     $route(to, from) {
@@ -41,4 +68,34 @@ export default {
 }
 </script>
 
-<style></style>
+<style scoped>
+h1 {
+  text-align: center;
+}
+h2.ex1 {
+  margin-left: 5cm;
+}
+h3.ex1 {
+  margin-left: 5cm;
+}
+img.ex1 {
+  margin-left: 5cm;
+}
+p {
+  text-align: right;
+}
+p.ex1 {
+  margin-right: 2cm;
+}
+p {
+  color: mediumpurple;
+}
+
+/*cc:change color*/
+a.cc:visited {
+  color: #0000ff;
+}
+a.cc:hover {
+  color: #ffcc00;
+}
+</style>
